ef589e147798cf33da5e36c2b150729bbb81cc30
petermunene <peter.munene@student.moringaschool.com>
2025-04-15 10:17:14 +0300

    c

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/phase-2-code-challenge/', // 👈 must match your GitHub repo name
   plugins: [react()],
 })
 
