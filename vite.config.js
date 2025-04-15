4f8ed74748dc9f337d6fd23023c3868b397c1467
petermunene <peter.munene@student.moringaschool.com>
2025-04-15 10:14:28 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/expense-tracker/', // 👈 must match your GitHub repo name
+  base: '/phase-2-code-challenge/', // 👈 must match your GitHub repo name
   plugins: [react()],
 })
 
