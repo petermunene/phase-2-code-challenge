8b0bf0ac149bd9ff03d218b2def1a141b72c0ca5
petermunene <peter.munene@student.moringaschool.com>
2025-04-15 14:11:23 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-// vite.config.js
+//vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/phase-2-code-challenge/', // <-- MUST match your GitHub repo name
+  build: {
+    outDir: 'docs' // Build directly into the docs folder
+  },
   plugins: [react()],
 })
+
 
